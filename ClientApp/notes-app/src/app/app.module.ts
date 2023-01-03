538de9e7394c823b5257fe9a6fdce854f15d14dc
Marcellov97 <magiu2012@me.com>
2023-01-03 10:33:05 +0100

    Creazione component HelloWorld in Angular

--- ClientApp/notes-app/src/app/app.module.ts
+++ ClientApp/notes-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
