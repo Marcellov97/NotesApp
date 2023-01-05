672543d5430e44eb3b9fd528d9636384c69f5bf4
Marcellov97 <magiu2012@me.com>
2023-01-05 11:19:59 +0100

    Implementato PostService, migliorato UtenteService

--- ClientApp/notes-app/src/app/service/utente.service.ts
+++ ClientApp/notes-app/src/app/service/utente.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { Observable, of } from 'rxjs';
 import {Utente} from "../model/utente";
-import {UTENTI} from "./DB-fake-utenti";
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +19,7 @@ export class UtenteService {
   }
 
   getUtente(nomeUtente : string): Observable<Utente>{
-    return this.http.get<Utente>(this.utenteURL + 'getUtente/' + nomeUtente)
+    return this.http.get<Utente>(this.utenteURL + '/getUtente/' + nomeUtente)
   }
 
 
