fad6b1cb4d701732142eb7d8527cf599832a1a7d
Marcellov97 <magiu2012@me.com>
2023-01-13 16:32:09 +0100

    Adesso si possono aggiungere e cancellare i commenti

--- ClientApp/notes-app/src/app/components/post/post.component.ts
+++ ClientApp/notes-app/src/app/components/post/post.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Post} from "../../model/post";
 import {CommentoService} from "../../service/commento.service";
 import {Commento} from "../../model/commento";
+import {AppStateService} from "../../service/app-state.service";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
@@ -14,17 +16,38 @@ export class PostComponent implements OnInit{
 
   commenti : Commento[] = [];
 
+  newCom : Commento = {
+    id: "", idPost: "", nomeUtente: "", testo: ""
+  }
 
-  constructor(private commentoService : CommentoService) {
+
+  constructor(private commentoService : CommentoService, private appState : AppStateService) {
   }
 
   ngOnInit(): void {
     this.commentoService.getCommentiByPost(this.post.id).subscribe((data : Commento[]) => {
       this.commenti = data;
     })
-
   }
 
+  myNickName(){
+    return this.appState.getNomeUtente();
+  }
+
+  deleteCommento(id : string){
+    this.commentoService.deleteCommento(id).subscribe();
+    window.location.reload();
+  }
+
+  newCommento(f : NgForm, idP : string){
+    this.newCom.idPost = idP;
+    this.newCom.nomeUtente = <string>this.myNickName();
+    this.newCom.testo = f.value.commento;
+
+    this.commentoService.setCommento(this.newCom).subscribe();
+
+    window.location.reload();
+  }
 
 
 }
