814ad006b9613b05cc2ebb3457bd32338c917385
Marcellov97 <magiu2012@me.com>
2023-01-17 16:09:10 +0100

    Bugfixing, adesso funziona il salvataggio della categoria nei nuovi post

--- ClientApp/notes-app/src/app/components/new-post/new-post.component.ts
+++ ClientApp/notes-app/src/app/components/new-post/new-post.component.ts
@@ -4,6 +4,7 @@ import {PostService} from "../../service/post.service";
 import {Post} from "../../model/post";
 import {AppStateService} from "../../service/app-state.service";
 import {Categorie} from "src/app/shared/categorie";
+import * as events from "events";
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
@@ -17,10 +18,13 @@ export class NewPostComponent implements OnInit{
     'esercizi svolti',
     'risultati esercizi'
   ]
+  
   newPost : Post = {
     categoria: [],
     descrizione: "", id: "", nomeUtente: "", segnalato: false, valutazione: 0
   }
+
+  categoriaSelected : string = ''
 
 
   constructor(private postService : PostService, private appState : AppStateService) {
@@ -29,12 +33,18 @@ export class NewPostComponent implements OnInit{
   postMessage(f : NgForm){
     this.newPost.nomeUtente = <string>this.appState.getNomeUtente();
     this.newPost.descrizione = f.value.message;
+    this.newPost.categoria.push(this.categoriaSelected);
 
     this.postService.setPost(this.newPost).subscribe();
 
     window.location.reload();
   }
 
+  changeCategoria(data : string){
+    this.categoriaSelected = data;
+  }
+
   ngOnInit(): void {
+
   }
 }
--- ClientApp/notes-app/src/app/shared/categorie.ts
+++ ClientApp/notes-app/src/app/shared/categorie.ts
@@ -1,6 +1,6 @@
 export class Categorie{
 
-  categs : string[] = [
+   categs : string[] = [
     'video',
     'appunti',
     'esercizi',
